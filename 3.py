f8f2c9c00d7ec324c38ebc0aec1e441b53b95ec4
chineseflava <bjorn.walter66@gmail.com>
2023-12-03 11:16:26 +0100

    Day 3 completed...

--- 3.py
+++ 3.py
@@ -1,58 +1,73 @@
 import re
 import numpy as np
 
-def contains_symbols(input_string, symbols="+$*#@/&-%="):
-    for char in input_string:
-        if char in symbols:
-            return True
+def is_symbol(x):
+    symbols = "+$#@/&-%=*"
+    if x in symbols:
+        return True
     return False
 
 def main():
     with open("inputs/3.txt", "r") as file:
         lines = file.readlines()
     schematic = []
-    errors = []
-    #numbers = []
-    #remove_symbols = "+$*#"
+    part_numbers = []
+    gear_ratios = []
+
     for idx, line in enumerate(lines):
         if idx == 0:
-            schematic.append("."*len(line))
-        # for x in [x for x in re.split(r"[*.#+$]",line.strip("\n")) if x]:
-        #     numbers.append(int(x))
+            schematic.append("."*(len(line)+2))
+
         schematic_line = line.strip("\n").split()
         schematic.append("."+schematic_line[0]+".")
         if idx == len(lines)-1:
-            schematic.append("."*len(line))
-    print(schematic)
-    #print(numbers)
-    for j, y in enumerate(schematic):
-        skip = 0
-        for i, x in enumerate(y):
-            if skip != 0:
-                skip -= 1
-                continue
-            if x.isdigit():
-                number = x
-                i += 1
-                while y[i].isdigit():
-                    number += y[i]
-                    i += 1
-                # Replace the part of the schematic with erroneous numbers.
-                char_list = list(schematic[j])
-                char_list[i-len(number):i] = "."*len(number)
-                schematic[j] = ''.join(char_list)
-                skip = len(number)
+            schematic.append("."*(len(line)+2))
 
-                # Check surrounding elements for symbols
-                above = schematic[j-1][i-len(number)-1:i+1]
-                middle = schematic[j][i-len(number)-1:i+1]
-                below = schematic[j+1][i-len(number)-1:i+1]
-                print(above, middle, below)
-                if contains_symbols(above+middle+below,"+$*#@/&-%="):
-                    errors.append(int(number))                     
-    # print(schematic)
-    # print(errors)
-    print(f"Answer: {sum(errors)}")
+    for j, y in enumerate(schematic):
+        for i, x in enumerate(y):
+            if is_symbol(x):
+                # Part 1
+                numbers = []
+                for n in range(j-1,j+2):
+                    for m in range(i-1,i+2):
+                         pos = schematic[n][m]
+                         #print(pos)
+                         if pos.isdigit():
+                            number = pos
+                            idx = m-1
+                            #start = idx
+                            while schematic[n][idx].isdigit():
+                                number = schematic[n][idx] + number
+                                idx -= 1
+                                
+                                
+                            idx = m+1
+                            while schematic[n][idx].isdigit():
+                                number = number + schematic[n][idx]
+                                idx += 1
+                            start = idx-len(number)
+                            numbers.append(number)
+
+                            # Remove number from schematic.
+                            print(schematic[n])
+                            char_list = list(schematic[n])
+                            char_list[start:start+len(number)] = "."*len(number)
+                            schematic[n] = ''.join(char_list)
+                part_numbers.append(int(number))   
+
+                # Part 2
+                if x == "*" and len(numbers)>1:
+                    gear_ratio = int(numbers[0])*int(numbers[1])
+                    gear_ratios.append(int(gear_ratio))
+                    #print(part_number)
+
+                
+            
+    for item in schematic:
+        print(item)
+    print(part_numbers)
+    print(f"Answer 1: {sum(part_numbers)}")
+    print(f"Answer 2: {sum(gear_ratios)}")
 
 if __name__ == "__main__":
     main()
