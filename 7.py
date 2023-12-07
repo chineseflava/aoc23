60b646a340e50806c09ae78e4e27680bd2fc5b2d
Björn Walter <bjornwa@axis.com>
2023-12-07 09:13:18 +0100

    Forgot to commit all saves...

--- 7.py
+++ 7.py
@@ -5,21 +5,32 @@ class Hand:
         self.bet = int(line[1])
         self.type = self.determine_type(self.cards)
 
-   
-
     def sort_cards(self, string):
-        cards = {}
+        cards = [0]*14
         card_translator = {"T":10, "J":11, "Q":12, "K":13, "A":14}
         for letter in string:
-            if letter.isdigit():
-                cards[letter] += 1
-            else:
-                cards[card_translator[letter]] += 1
-
-        return
+            if letter.isdigit(): cards[int(letter)] += 1
+            else: cards[card_translator[letter]] += 1
+        return cards
     
+    # Return type and the card value of type.
     def determine_type(self):
-        return
+        if 5 in self.cards:
+            return 7, self.cards.key()
+        if 4 in self.cards.values():
+            return 6
+        elif 3 in self.cards.values():
+            # Add case with full house.
+            return 5
+            return 4
+        elif 2 in self.cards.value():
+            # Add case with two pairs.
+            return 3
+            # One pair
+            return 2
+        else:
+            # High card
+            return 1
 
     def compare_hands(self, hand):
         # Start with comparing type
