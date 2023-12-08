64720219d30b5619d993c8c9048caeffdf4b2824
Björn Walter <bjornwa@axis.com>
2023-12-08 08:47:37 +0100

    more Day 7...
    Change-Id: I7636017c3d0ec89ff10df40e8ccbc628e3a83b72

--- 7.py
+++ 7.py
@@ -1,41 +1,59 @@
 
 class Hand:
     def __init__(self, line):
-        self.cards = self.sort_cards(line[0])
+        self.cards, self.sorted_cards = self.sort_cards(line[0])
         self.bet = int(line[1])
-        self.type = self.determine_type(self.cards)
+        self.type, self.type_val = self.determine_type()
 
     def sort_cards(self, string):
-        cards = [0]*14
+        sorted_cards = [0]*15
+        cards = []
         card_translator = {"T":10, "J":11, "Q":12, "K":13, "A":14}
         for letter in string:
-            if letter.isdigit(): cards[int(letter)] += 1
-            else: cards[card_translator[letter]] += 1
-        return cards
+            if letter.isdigit(): 
+                sorted_cards[int(letter)] += 1
+                cards.append(int(letter))
+            else: 
+                sorted_cards[card_translator[letter]] += 1
+                cards.append(card_translator[letter])
+        return cards, sorted_cards
     
     # Return type and the card value of type.
     def determine_type(self):
-        if 5 in self.cards:
-            return 7, self.cards.key()
-        if 4 in self.cards.values():
-            return 6
-        elif 3 in self.cards.values():
+        if 5 in self.sorted_cards:
+            return 7, self.sorted_cards.index(5)
+        elif 4 in self.sorted_cards:
+            return 6, self.sorted_cards.index(4)
+        elif 3 in self.sorted_cards:
             # Add case with full house.
-            return 5
-            return 4
-        elif 2 in self.cards.value():
+            if 2 in self.sorted_cards:
+                return 5, [self.sorted_cards.index(3), self.sorted_cards.index(2)]
+            else: 
+                return 4, self.sorted_cards.index(3)
+        elif 2 in self.sorted_cards:
+            indices = [i for i, value in enumerate(self.sorted_cards) if value == 2]
             # Add case with two pairs.
-            return 3
+            if len(indices) > 1: 
+                return 3, indices
             # One pair
-            return 2
+            else: 
+                return 2, self.sorted_cards.index(2)
         else:
             # High card
             return 1
 
     def compare_hands(self, hand):
         # Start with comparing type
+        if self.type > hand.type: return True
         # Make cases for comparing types if they are the same.
-        return -1
+        if self.type == hand.type:
+            for i, _ in enumerate(self.cards):
+                if self.cards[i] != hand.cards[i]:
+                    return self.cards[i] > hand.cards[i]
+        else:
+            return False
+        
+        
     
 
 def main():
@@ -49,7 +67,7 @@ def main():
     for hand in hands:
         for idx, rank in enumerate(ranking):
             if hand.compare_hands(rank) == -1:
-                ranking.inser(idx, hand)
+                ranking.insert(idx, hand)
                 break
 
 
