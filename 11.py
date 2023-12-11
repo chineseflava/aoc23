585df71657b9126ec3d374b3a0a271afbaae9c41
Björn Walter <bjornwa@axis.com>
2023-12-11 12:20:13 +0100

    Complete day 11
    Change-Id: I8f67aa620fda6f4de21c4985cf7b980f263a461c

--- 11.py
+++ 11.py
@@ -18,16 +18,64 @@ def expand_universe(uni):
     for i in expand_row_idxs:
         uni = np.insert(uni, i, ".", axis=0)
     
-    return uni
+    return uni, expand_col_idxs, expand_row_idxs
 
-def min_dist(uni, source, )
+def min_dist(uni, source, dest):
+    source_idx = np.where(uni == source)
+    dest_idx = np.where(uni == dest)
+
+    row_range = [source_idx[0], dest_idx[0]]
+    col_range = [source_idx[1], dest_idx[1]]
+
+    row_range.sort()
+    col_range.sort()
+
+    dist = abs(source_idx[0]-dest_idx[0])+abs(source_idx[1]-dest_idx[1])
+    #print(source)
+    return int(dist), row_range, col_range
+    
 
 def main():
-    with open("inputs/11_test.txt", "r") as file:
+    with open("inputs/11.txt", "r") as file:
         lines = file.readlines()
-    universe = np.array([[e for e in line.strip("\n")] for line in lines])
+    universe = np.array([[e for e in line.strip("\n")] for line in lines], dtype='U25')
+    star_count = 1
+    for row in range(universe.shape[0]):
+        for col in range(universe.shape[1]):
+            
+            if universe[row, col] == "#":
+                universe[row,col] = str(star_count)
+                star_count += 1
 
-    universe = expand_universe(universe)
+
+    expanded_universe, expanded_cols, expanded_rows = expand_universe(universe)
+    print(universe)
+    dists = []
+    # Part 1
+    # for i in range(star_count):
+    #     for j in range(i+1,star_count):
+    #         dist, _, _ = min_dist(expanded_universe, f"{i+1}", f"{j}")
+    #         dists.append(dist)
+    # print(sum(dists))
+
+    # Part 2
+    dists2 = []
+    for i in range(star_count):
+        for j in range(i+1,star_count):
+            dist, row_range, col_range = min_dist(universe, f"{i+1}", f"{j}")
+            
+            for m in expanded_cols:
+                if m < col_range[1] and m > col_range[0]:
+                    dist += 999999
+            for m in expanded_rows:
+                if m < row_range[1] and m > row_range[0]:
+                    dist += 999999
+            dists2.append(dist)
+    
+    print(sum(dists2))
+
+    #print(min_dist(universe, "5", "9"))
+
 
 
 if __name__ == "__main__":
