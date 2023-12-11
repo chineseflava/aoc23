6af1a38a1c23743f30f81257cb362161f4b3c838
Björn Walter <bjornwa@axis.com>
2023-12-11 16:33:55 +0100

    Start day 10
    Change-Id: Ib2a854fee96b293413f584895a706cf3354c7368

---
+++ 10.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def traverse
+
+def main():
+    with open("inputs/10_test.txt", "r") as file:
+        lines = file.readlines()
+    grid = np.array([[e for e in line.strip("\n")] for line in lines], dtype='U25')
+    print(grid)
+    start = np.where(grid == 'S')
+    print(start)
+
+if __name__ == "__main__":
+    main()
--- 7.py
+++ 7.py
@@ -3,12 +3,12 @@ class Hand:
     def __init__(self, line):
         self.cards, self.sorted_cards = self.sort_cards(line[0])
         self.bet = int(line[1])
-        self.type, self.type_val = self.determine_type()
+        self.type = self.determine_type()
 
     def sort_cards(self, string):
         sorted_cards = [0]*15
         cards = []
-        card_translator = {"T":10, "J":11, "Q":12, "K":13, "A":14}
+        card_translator = {"T":10, "J":1, "Q":12, "K":13, "A":14}
         for letter in string:
             if letter.isdigit(): 
                 sorted_cards[int(letter)] += 1
@@ -20,27 +20,42 @@ class Hand:
     
     # Return type and the card value of type.
     def determine_type(self):
+        jokers = self.sorted_cards[1]
         if 5 in self.sorted_cards:
-            return 7, self.sorted_cards.index(5)
+            return 7
         elif 4 in self.sorted_cards:
-            return 6, self.sorted_cards.index(4)
+            if jokers == 1:
+                return 7
+            else:
+                return 6
         elif 3 in self.sorted_cards:
+            if jokers == 1:
+                return 6
+            elif jokers == 2:
+                return 7
             # Add case with full house.
-            if 2 in self.sorted_cards:
-                return 5, [self.sorted_cards.index(3), self.sorted_cards.index(2)]
+            elif 2 in self.sorted_cards:
+                return 5
             else: 
-                return 4, self.sorted_cards.index(3)
+                return 4
         elif 2 in self.sorted_cards:
             indices = [i for i, value in enumerate(self.sorted_cards) if value == 2]
+            if jokers == 1:
+                return 5
+            elif jokers == 2:
+                return 6
             # Add case with two pairs.
-            if len(indices) > 1: 
-                return 3, indices
+            elif len(indices) > 1: 
+                return 3
             # One pair
-            else: 
-                return 2, self.sorted_cards.index(2)
+            elif len(indices) == 1: 
+                return 2
         else:
+            if jokers == 1:
+                return 2
+            else:        
             # High card
-            return 1, None
+                return 1
 
     def compare_hands(self, hand):
         # Start with comparing type
@@ -57,7 +72,7 @@ class Hand:
     
 
 def main():
-    with open("inputs/7.txt", "r") as file:
+    with open("inputs/7_test.txt", "r") as file:
         lines = file.readlines()
     hands = []
     for line in lines:
